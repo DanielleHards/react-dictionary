80ab5d6ea036b9091a64b0c66f9550d77a4aead4
DanielleHards <124166684+DanielleHards@users.noreply.github.com>
2023-10-05 20:54:47 -0400

    added an image to app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
+import book from "./book.png";
 
 function App() {
   return (
     <div className="App">
-      Hello!
       <header className="App-header"></header>
+      <img src={book} className="book-img img-fluid" alt="cartoon book lover" />
     </div>
   );
 }
