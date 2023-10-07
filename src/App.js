9007747886e7d339c8d996d5d1eb7fb12211995d
DanielleHards <124166684+DanielleHards@users.noreply.github.com>
2023-10-06 20:19:19 -0400

    added search engine functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import "./App.css";
 import book from "./book.png";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      <img src={book} className="book-img img-fluid" alt="cartoon book lover" />
+      <div className="container">
+        <header className="App-header">
+          <img
+            src={book}
+            className="book-img img-fluid"
+            alt="cartoon book lover"
+          />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center">
+          Coded by <span className="studentName">Danielle Hards</span> for{" "}
+          <span className="sheCodes">SheCodes</span>
+        </footer>
+      </div>
     </div>
   );
 }
