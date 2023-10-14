d0059c5b43f1964132118fff9d83ba1e0987516a
DanielleHards <124166684+DanielleHards@users.noreply.github.com>
2023-10-13 20:02:13 -0400

    edited labels for display

--- src/Meaning.js
+++ src/Meaning.js
@@ -6,9 +6,14 @@ export default function Meaning(props) {
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
       <p>
+        <strong>Definition:</strong>
         {props.meaning.definition}
         <br />
+        <strong>Example:</strong>
         {props.meaning.example}
+        <br />
+        <strong>Synonyms:</strong>
+        {props.meaning.synonyms}
       </p>
     </div>
   );
