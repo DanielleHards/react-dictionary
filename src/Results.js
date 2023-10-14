be5b3a4d7b55cee109688c5ae7c60b59fe0c88e8
DanielleHards <124166684+DanielleHards@users.noreply.github.com>
2023-10-14 10:18:25 -0400

    deleted phonetic component

--- src/Results.js
+++ src/Results.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Meaning from "./Meaning";
-import Phonetic from "./Phonetic";
 
 export default function Results(props) {
   if (props.results) {
@@ -8,13 +7,7 @@ export default function Results(props) {
       <div className="Results">
         <section>
           <h2>{props.results.word}</h2>
-          {props.results.phonetic.map(function (phonetic, index) {
-            return (
-              <div key={index}>
-                <Phonetic phonetic={phonetic} />
-              </div>
-            );
-          })}
+          <h3>/{props.results.phonetic}/</h3>
         </section>
         {props.results.meanings.map(function (meaning, index) {
           return (
